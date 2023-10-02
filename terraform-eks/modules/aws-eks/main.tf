776af37e4b325768ed93765567c79721201a9b02
AdvanceCodeMarshall <mgmbcs.santosh@gmail.com>
2023-10-03 00:02:40 +0530

    fixed

--- terraform-eks/modules/aws-eks/main.tf
+++ terraform-eks/modules/aws-eks/main.tf
@@ -39,20 +39,16 @@ resource "aws_iam_role" "devops_eks_cluster_iam_role" {
     # The policy that grants an entity permission to assume the role.
     # Used to access AWS resources that you might not normally have access to.
     # The role that Amazon EKS will use to create AWS resources for Kubernetes clusters
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
