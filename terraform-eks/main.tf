042923324601f75ffef9498bd8ad7cfbc53babc4
AdvanceCodeMarshall <mgmbcs.santosh@gmail.com>
2023-10-02 22:45:19 +0530

    fixed name

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -10,7 +10,7 @@ module "aws_eks_node_group" {
     source = "./modules/aws-eks-nodegroup"
     for_each = var.devops_aws_eks_node_group_config
     aws_eks_node_group_name         = each.value.aws_eks_node_group_name
-    aws_eks_cluster_name            = module.devops_aws_eks_cluster[each.value.aws_eks_cluster_name].eks_cluster
+    aws_eks_cluster_name            = module.devops_aws_eks_cluster[each.value.aws_eks_cluster_name].created_eks_cluster_name
     aws_eks_node_group_subnet_ids   = each.value.aws_eks_node_group_subnet_ids
     aws_eks_node_group_iam_role     = each.value.aws_eks_node_group_iam_role
     aws_eks_node_group_tag          = each.value.aws_eks_node_group_tag
